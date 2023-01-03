237df1cf92860cecd8e4b2e861624b9e03a23371
miracleonyenma <miracleficient@gmail.com>
2023-01-03 22:59:50 +0100

    update: auth pages

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,17 +1,17 @@
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
-import '../styles/auth.css'
+import "../styles/auth.css";
 
 export default function Login() {
-
   // Remove the page navigation from the login page
   useEffect(() => {
-    const pageNav = document.getElementById('pageNav')
-    pageNav.style.display = 'none'
-  }, [])
+    const pageNav = document.getElementById("pageNav");
+    pageNav.style.display = "none";
+  }, []);
 
   return (
-    <div className="a-right">
+    <section className="auth-section">
+      {/* <div className="a-right">
       <form className="infoForm authForm">
         <h3>Log In</h3>
 
@@ -43,6 +43,47 @@ export default function Login() {
           </button>
         </div>
       </form>
-    </div>
+    </div> */}
+
+      <div className="auth-section__wrapper">
+        <header className="auth-header">
+          <h1>Log In</h1>
+        </header>
+        <div className="form-cont">
+          <form className="auth-form">
+            <div className="form__wrapper">
+              <div className="form-control">
+                <label htmlFor="firstname">Username</label>
+                <input
+                  className="infoInput"
+                  name="username"
+                  placeholder="Username"
+                  type="text"
+                />
+              </div>
+
+              <div className="form-control">
+                <label htmlFor="password">Password</label>
+                <input
+                  className="infoInput"
+                  name="password"
+                  placeholder="password"
+                  type="text"
+                />
+              </div>
+
+              <div className="action-cont">
+                <button className="button">Log in</button>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div className="info-cont">
+          <p>
+            Don't have an account? <Link to="/signup">Sign Up</Link>
+          </p>
+        </div>
+      </div>
+    </section>
   );
 }
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,17 +1,17 @@
-import { useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import '../styles/auth.css'
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import "../styles/auth.css";
 
 export default function SignUp() {
-
   // Remove the page navigation from the signup page
   useEffect(() => {
-    const pageNav = document.getElementById('pageNav')
-    pageNav.style.display = 'none'
-  }, [])
+    const pageNav = document.getElementById("pageNav");
+    pageNav.style.display = "none";
+  }, []);
 
   return (
-    <div className="a-right">
+    <section className="auth-section">
+      {/* <div className="a-right">
       <form className="infoForm authForm">
         <h3>Sign up</h3>
         <div>
@@ -43,7 +43,65 @@ export default function SignUp() {
         </div>
         <button className="button infoButton" type="submit">Signup</button>
       </form>
-    </div>
-  );
+    </div> */}
 
+      <div className="auth-section__wrapper">
+        <header className="auth-header">
+          <h1>Sign Up</h1>
+        </header>
+        <div className="form-cont">
+          <form className="auth-form">
+            <div className="form__wrapper">
+              <div className="form-group">
+                <div className="form-control">
+                  <label htmlFor="name">Name</label>
+                  <input
+                    className="infoInput"
+                    name="name"
+                    placeholder="name"
+                    type="text"
+                  />
+                </div>
+                <div className="form-control">
+                  <label htmlFor="firstname">Username</label>
+                  <input
+                    className="infoInput"
+                    name="username"
+                    placeholder="Username"
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div className="form-control">
+                <label htmlFor="password">Password</label>
+                <input
+                  className="infoInput"
+                  name="password"
+                  placeholder="password"
+                  type="text"
+                />
+              </div>
+              <div className="form-control">
+                <label htmlFor="confirm-password">Confirm Password</label>
+                <input
+                  className="infoInput"
+                  name="confirm-password"
+                  placeholder="confirm-password"
+                  type="text"
+                />
+              </div>
+              <div className="action-cont">
+                <button className="button">Create account</button>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div className="info-cont">
+          <p>
+            Have an account? <Link to='/login'>Log in</Link>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
